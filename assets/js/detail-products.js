a2c99ad10b140b941639ad698018d88426ba67c6
Patricia Ho <hopatricia1711@gmail.com>
2024-01-20 00:40:36 +0700

    add json and fix the filtering + make the content dynamic

---
+++ assets/js/detail-products.js
@@ -0,0 +1,118 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to parse URL parameters
+    function getQueryParam(param) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(param);
+    }
+
+    function initializeSwipers() {
+        var swiper = new Swiper(".mySwiper", {
+            spaceBetween: 10,
+            slidesPerView: 3,
+            freeMode: true,
+            watchSlidesProgress: true,
+        });
+    
+        var swiper2 = new Swiper(".mySwiper2", {
+            spaceBetween: 10,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            thumbs: {
+                swiper: swiper,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                type: "fraction",
+            },
+        });
+    }
+
+    // Fetch and display product data
+    function fetchAndDisplayProduct(productId, productName) {
+        fetch('../../assets/data/products.json')
+            .then(response => response.json())
+            .then(products => {
+                const product = products.find(p => p.id == productId || p.product_name == productName);
+                if (product) {
+                    displayProductDetails(product);
+                } else {
+                    console.error('Product not found');
+                    // Handle case where product is not found
+                }
+            })
+            .catch(error => console.error('Error fetching products:', error));
+    }
+
+    // Display product details in HTML
+    function displayProductDetails(product) {
+        document.querySelector('h1.text-h2').textContent = product.product_name;
+        if (product.description.length > 0) {
+            document.querySelector('.product-description p').innerHTML = product.description;
+        }
+
+        if (product.note.length > 0) {
+            document.querySelector('.product-notes p').innerHTML= product.note;
+        }
+
+        const swiperWrappers = document.querySelectorAll('.swiper .swiper-wrapper');
+        if (product.images && Array.isArray(product.images)) {
+            swiperWrappers.forEach(swiperWrapper => {
+                // Clear existing slides
+                swiperWrapper.innerHTML = '';
+        
+                product.images.forEach(image => {
+                    const listItem = document.createElement('div');
+                    listItem.className = 'swiper-slide';
+        
+                    const img = document.createElement('img');
+                    img.src = image;
+        
+                    listItem.appendChild(img);
+                    swiperWrapper.appendChild(listItem);
+                });
+            });
+
+            // Destroy existing Swiper instances if they exist
+            if (window.swiper && window.swiper2) {
+                window.swiper.destroy(true, true);
+                window.swiper2.destroy(true, true);
+            }
+
+            // Reinitialize Swipers
+            initializeSwipers();
+        }
+        
+
+        const benefitsList = document.querySelector('.product-strength ul');
+        if (product.benefits && Array.isArray(product.benefits)) {
+            benefitsList.innerHTML = ''; // Clear existing list items
+            product.benefits.forEach(benefit => {
+                const listItem = document.createElement('li');
+                listItem.className = 'text-p flex items-start gap-5';
+        
+                const icon = document.createElement('i');
+                icon.className = 'material-icons-round';
+                icon.textContent = 'done';
+        
+                listItem.appendChild(icon);
+                listItem.appendChild(document.createTextNode(benefit));
+        
+                benefitsList.appendChild(listItem);
+            });
+        }
+        // Add more fields as needed...
+
+        // Update image gallery if applicable
+        
+
+        // Reinitialize Swiper here if necessary
+    }
+
+    // Get product ID or name from URL
+    const productId = getQueryParam('id');
+    const productName = getQueryParam('name');
+    fetchAndDisplayProduct(productId);
+    displayProductDetails(productId);
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,77 @@ for (i = 0; i < accordion.length; i++) {
         this.classList.toggle('active')
     })
 }
+
+// document.querySelectorAll('.product-item').forEach(item => {
+//     item.addEventListener('click', function() {
+//         const productId = this.getAttribute('data-product-id'); // Assuming each product has a 'data-product-id' attribute
+//         window.location.href = `./details-product.html?productId=${productId}`;
+//     });
+// });
+
+function fetchProducts(callback) {
+    const url = "../assets/data/products.json";
+    fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            const productsContainer = document.getElementById('all-products'); // Assuming you have a container with this ID
+            data.forEach(product => {
+                const productElement = createProductElement(product);
+                productsContainer.appendChild(productElement);
+            });
+
+            if (callback) callback();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+function createProductElement(product) {
+    const anchor = document.createElement('a');
+    const encodedProductName = encodeURIComponent(product.product_name); // Encode the product name to make it URL-safe
+    anchor.href = `./details-product/detail-product.html?id=${product.id}&name=${encodedProductName}`; 
+    anchor.className = `product-item ${product.type_class} md:col-span-4 col-span-12 flex flex-col gap-4 border rounded-lg`;
+    // anchor.dataset.category = product.type_class;
+
+    const imageDiv = document.createElement('div');
+    imageDiv.className = 'rounded-t-lg overflow-hidden w-100 h-[240px] max-h-[240px] min-h-[240px] object-fill';
+
+    const img = document.createElement('img');
+    img.src = product.images[0]; // Assuming the first image is what you want to display
+    img.className = 'w-full h-full object-cover';
+    img.alt = product.product_name;
+
+    imageDiv.appendChild(img);
+
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'px-5 py-2';
+
+    const categoryDiv = document.createElement('div');
+    categoryDiv.className = 'categories flex gap-2';
+
+    // const badge = document.createElement('span');
+    // badge.className = 'badge-best-seller text-subtitle font-medium'; // Modify as needed
+    // badge.textContent = 'Best Seller'; // Modify as needed
+
+    const categorySpan = document.createElement('span');
+    categorySpan.className = 'badge-primary text-subtitle font-medium'; // Modify as needed
+    categorySpan.textContent = product.category;
+
+    // categoryDiv.appendChild(badge);
+    categoryDiv.appendChild(categorySpan);
+
+    const productName = document.createElement('h3');
+    productName.className = 'text-h3 font-semibold py-3';
+    productName.textContent = product.product_name;
+
+    contentDiv.appendChild(categoryDiv);
+    contentDiv.appendChild(productName);
+
+    anchor.appendChild(imageDiv);
+    anchor.appendChild(contentDiv);
+
+    return anchor;
+}
